9f374fb23e14c7fc63a4c2e5e83f12976341f128
Funix565 <denkin.dmitriy@gmail.com>
2025-01-18 02:20:44 +0200

    2683. Neighboring Bitwise XOR. Solved via simulation

---
+++ LeetCodeTasks/NeighboringBitwiseXOR.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCodeTasks
+{
+    // Link: https://leetcode.com/problems/neighboring-bitwise-xor/
+
+    public class NeighboringBitwiseXOR
+    {
+        public static bool Solve(int[] derived)
+        {
+            if (derived.Length == 1 && derived[0] == 0)
+            {
+                return true;
+            }
+
+            if (derived.Length == 1)
+            {
+                return false;
+            }
+
+            int start = 0;
+            for (int i = 0; i < derived.Length - 1; ++i)
+            {
+                start = start ^ derived[i];
+            }
+
+            return (start ^ 0) == derived[derived.Length - 1];
+        }
+    }
+}
