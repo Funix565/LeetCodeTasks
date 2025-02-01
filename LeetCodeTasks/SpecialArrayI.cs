f4a7e68954f9255388a5a28d0ad56b29188574f1
Funix565 <denkin.dmitriy@gmail.com>
2025-02-02 00:54:36 +0200

    3151. Special Array I. Solved with bitwise approach to determine parities. odd + even = odd

---
+++ LeetCodeTasks/SpecialArrayI.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeTasks
+{
+    // Link: https://leetcode.com/problems/special-array-i/
+
+    public class SpecialArrayI
+    {
+        public bool Solve(int[] nums)
+        {
+            if (nums.Length == 1)
+            {
+                return true;
+            }
+
+            for (int i = 0, j = 1; j < nums.Length; ++i, ++j)
+            {
+                if (((nums[i] + nums[j]) & 1) == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
