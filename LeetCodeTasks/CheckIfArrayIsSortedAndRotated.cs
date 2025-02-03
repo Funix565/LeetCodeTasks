2d814a43205814189b38629915c84b4bac8a3cfc
Funix565 <denkin.dmitriy@gmail.com>
2025-02-03 02:05:08 +0200

    1752. Check if Array Is Sorted and Rotated. Solved after some wrong attempts, experiment with edge cases

---
+++ LeetCodeTasks/CheckIfArrayIsSortedAndRotated.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeTasks
+{
+    // Link: https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/
+
+    public class CheckIfArrayIsSortedAndRotated
+    {
+        public bool Check(int[] nums)
+        {
+            for (int i = 0; i < nums.Length - 1; ++i)
+            {
+                if (nums[i + 1] < nums[i])
+                {
+                    if (nums[i + 1] > nums[0])
+                    {
+                        return false;
+                    }
+
+                    for (int j = i + 1; j < nums.Length - 1; ++j)
+                    {
+                        if (nums[j + 1] < nums[j] || nums[j + 1] > nums[0])
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
