2f39d375536258ac362853e2be018b77d7b9ef11
Funix565 <denkin.dmitriy@gmail.com>
2025-02-05 00:09:17 +0200

    1800. Maximum Ascending Subarray Sum. Quick first try with linear scan

---
+++ LeetCodeTasks/MaximumAscendingSubarraySum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LeetCodeTasks
+{
+    // Link: https://leetcode.com/problems/maximum-ascending-subarray-sum/
+
+    public class MaximumAscendingSubarraySum
+    {
+        public int MaxAscendingSum(int[] nums)
+        {
+            int answer = nums[0];
+            int sum = nums[0];
+            for (int i = 0; i < nums.Length - 1; ++i)
+            {
+                if (nums[i] < nums[i + 1])
+                {
+                    sum += nums[i + 1];
+                }
+                else
+                {
+                    answer = Math.Max(answer, sum);
+                    sum = nums[i + 1];
+                }
+            }
+
+            return Math.Max(answer, sum);
+        }
+    }
+}
