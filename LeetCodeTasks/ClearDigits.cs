6c7ab3b670c702257f5062d0002332fd21dfb97c
Funix565 <denkin.dmitriy@gmail.com>
2025-02-11 01:00:10 +0200

    3174. Clear Digits

---
+++ LeetCodeTasks/ClearDigits.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace LeetCodeTasks
+{
+    // Link: https://leetcode.com/problems/clear-digits/
+
+    public class ClearDigits
+    {
+        public string Solve(string s)
+        {
+            StringBuilder answer = new StringBuilder();
+
+            foreach (char c in s)
+            {
+                if (char.IsDigit(c) && answer.Length > 0)
+                {
+                    answer.Remove(answer.Length - 1, 1);
+                }
+                else
+                {
+                    answer.Append(c);
+                }
+            }
+
+            return answer.ToString();
+        }
+    }
+}
