bf74f17945edcee27b30f2068a9d2319f6e93d01
Funix565 <denkin.dmitriy@gmail.com>
2025-02-14 00:44:02 +0200

    2342. Max Sum of a Pair With Equal Sum of Digits. Add better approaches

--- LeetCodeTasks/MaxSumOfPairWithEqualSumOfDigits.cs
+++ LeetCodeTasks/MaxSumOfPairWithEqualSumOfDigits.cs
@@ -7,6 +7,159 @@ namespace LeetCodeTasks
 
     public class MaxSumOfPairWithEqualSumOfDigits
     {
+        public int MaximumSum_PlaiArraysOnePass(int[] nums)
+        {
+            int answer = -1;
+
+            int largestDigitSum = SumDigits(999999999);
+
+            int[][] digitSumToNumbers = new int[largestDigitSum + 1][];
+
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                int digitSum = SumDigits(nums[i]);
+
+                if (digitSumToNumbers[digitSum] == null)
+                {
+                    digitSumToNumbers[digitSum] = [-1, -1];
+                }
+
+                // If the current number is at least as large as one of the two stored values, consider updating
+                if (nums[i] >= digitSumToNumbers[digitSum][1])
+                {
+                    // If it's larger than the first max, shift both values down
+                    if (nums[i] > digitSumToNumbers[digitSum][0])
+                    {
+                        digitSumToNumbers[digitSum][1] = digitSumToNumbers[digitSum][0];
+                        digitSumToNumbers[digitSum][0] = nums[i];
+                    }
+                    // Otherwise, update only the second max
+                    else
+                    {
+                        digitSumToNumbers[digitSum][1] = nums[i];
+                    }
+                }
+
+                // We can integrate the second loop logic directly in the main loop.
+                // The intention is the same - calculate sum of a pair.
+                // And also we operate the actual values immediately, without searching all array and skipping nulls.
+                int[] pq = digitSumToNumbers[digitSum];
+
+                if (pq != null && pq[1] != -1 && pq[0] != -1)
+                {
+                    int first = pq[0];
+                    int second = pq[1];
+                    answer = Math.Max(answer, first + second);
+                }
+            }
+
+            //foreach (int[] pq in digitSumToNumbers)
+            //{
+            //    if (pq != null && pq[1] != -1 && pq[0] != -1)
+            //    {
+            //        int i = pq[0];
+            //        int j = pq[1];
+            //        answer = Math.Max(answer, i + j);
+            //    }
+            //}
+
+            return answer;
+        }
+
+        public int MaximumSum_PlainArrays(int[] nums)
+        {
+            int answer = -1;
+
+            int largestDigitSum = SumDigits(999999999);
+
+            int[][] digitSumToNumbers = new int[largestDigitSum + 1][];
+
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                int digitSum = SumDigits(nums[i]);
+
+                if (digitSumToNumbers[digitSum] == null)
+                {
+                    digitSumToNumbers[digitSum] = [-1, -1];
+                }
+
+                // If the current number is at least as large as one of the two stored values, consider updating
+                if (nums[i] >= digitSumToNumbers[digitSum][1])
+                {
+                    // If it's larger than the first max, shift both values down
+                    if (nums[i] > digitSumToNumbers[digitSum][0])
+                    {
+                        digitSumToNumbers[digitSum][1] = digitSumToNumbers[digitSum][0];
+                        digitSumToNumbers[digitSum][0] = nums[i];
+                    }
+                    // Otherwise, update only the second max
+                    else
+                    {
+                        digitSumToNumbers[digitSum][1] = nums[i];
+                    }
+                }
+            }
+
+            foreach (int[] pq in digitSumToNumbers)
+            {
+                if (pq != null && pq[1] != -1 && pq[0] != -1)
+                {
+                    int i = pq[0];
+                    int j = pq[1];
+                    answer = Math.Max(answer, i + j);
+                }
+            }
+
+            return answer;
+        }
+
+        public static int MaximumSum_FrequencyArrayWithoutPriorityQueue(int[] nums)
+        {
+            int answer = -1;
+
+            int largestDigitSum = SumDigits(999999999);
+
+            int[][] digitSumToNumbers = new int[largestDigitSum + 1][];
+
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                int digitSum = SumDigits(nums[i]);
+
+                if (digitSumToNumbers[digitSum] == null)
+                {
+                    digitSumToNumbers[digitSum] = [-1, -1];
+                }
+
+                // If the current number is at least as large as one of the two stored values, consider updating
+                if (nums[i] >= digitSumToNumbers[digitSum][1])
+                {
+                    // If it's larger than the first max, shift both values down
+                    if (nums[i] > digitSumToNumbers[digitSum][0])
+                    {
+                        digitSumToNumbers[digitSum][1] = digitSumToNumbers[digitSum][0];
+                        digitSumToNumbers[digitSum][0] = nums[i];
+                    }
+                    // Otherwise, update only the second max
+                    else
+                    {
+                        digitSumToNumbers[digitSum][1] = nums[i];
+                    }
+                }
+
+                // Update the answer immediately after modifying the stored values, and avoid iterating over unused slots.
+                int[] pq = digitSumToNumbers[digitSum];
+
+                if (pq != null && pq[1] != -1 && pq[0] != -1)
+                {
+                    int first = pq[0];
+                    int second = pq[1];
+                    answer = Math.Max(answer, first + second);
+                }
+            }
+
+            return answer;
+        }
+
         public int MaximumSum(int[] nums)
         {
             int answer = -1;
